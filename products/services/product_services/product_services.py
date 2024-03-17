82810eaf7733e63f00967809853239464fc86ee5
animeshrick <animeshece198@gmail.com>
2024-03-17 18:45:08 +0530

    Updating update product feature

--- products/services/product_services/product_services.py
+++ products/services/product_services/product_services.py
@@ -1,10 +1,8 @@
 from django.core.exceptions import ObjectDoesNotExist
 from psycopg2 import DatabaseError
 from rest_framework.request import Request
-from ecom_exceptions.ecom_exceptions import ProductNotFoundError, ECOMValueError
-from products.models.db_models.category import Category
+from ecom_exceptions.ecom_exceptions import ProductNotFoundError
 from products.models.db_models.product import Product
-from products.models.db_models.sub_category import SubCategory
 from products.models.export_models.export_product import (
     ExportECOMProduct,
     ExportECOMProductList,
@@ -69,71 +67,6 @@ class ProductServices:
     def update_product_service(
         self, request_data: UpdateProductRequestType, request: Request
     ) -> ResponseData:
-        try:
-            seller_id = SellerServices().get_seller_id(request=request)
-            product = Product.objects.get(id=request_data.id, seller_id=seller_id)
-            category_type = Category.objects.filter(name=request_data.category).first()
-            sub_category_type: SubCategory = SubCategory.objects.filter(
-                name=request_data.subcategory
-            ).first()
-
-            if product:
-                if request_data.product_name:
-                    product.name = request_data.product_name
-
-                if request_data.brand:
-                    product.brand = request_data.brand
-
-                if request_data.count_in_stock:
-                    product.countInStock = request_data.count_in_stock
-
-                if request_data.category:
-                    if not category_type:
-                        raise ECOMValueError(msg="Category is not listed")
-                    else:
-                        product.category.name = request_data.category
-
-                if request_data.subcategory:
-                    if not sub_category_type:
-                        raise ECOMValueError(msg="Sub Category is not listed")
-                    elif sub_category_type.category != category_type:
-                        raise ECOMValueError(
-                            msg="Category & Sub Category are not matching"
-                        )
-                    else:
-                        product.subCategory.name = request_data.subcategory
-
-                if request_data.image:
-                    product.product_image = request_data.image
-
-                if request_data.product_image_list:
-                    product.product_image_list = request_data.product_image_list
-
-                if request_data.description:
-                    product.description = request_data.description
-
-                if request_data.actual_price:
-                    if request_data.actual_price <= 0:
-                        raise ECOMValueError(
-                            msg="Actual Price can not be zero or negative"
-                        )
-                    if request_data.actual_price >= 1000000:
-                        raise ECOMValueError(
-                            msg="Price can not be greater than Rs.9,99,999"
-                        )
-                    else:
-                        product.actual_price = request_data.actual_price
-
-                if request_data.offer_price:
-                    if request_data.offer_price > product.actual_price:
-                        raise ECOMValueError(
-                            msg="Offer Price can not be grater than actual price"
-                        )
-                    else:
-                        product.offer_price = request_data.offer_price
-
-                product.save()
-            return ResponseData(successMessage="Product has been updated successfully.")
-
-        except ObjectDoesNotExist:
-            raise ProductNotFoundError()
+        seller_id = SellerServices().get_seller_id(request=request)
+        request_data.update_product_in_db(seller_id=seller_id)
+        return ResponseData(successMessage="Product has been updated successfully.")
--- products/utils/helpers.py
+++ products/utils/helpers.py
@@ -1,4 +1,11 @@
 import uuid
+from typing import List
+
+from _decimal import Decimal
+
+from ecom_exceptions.ecom_exceptions import ECOMValueError
+from products.models.db_models.category import Category
+from products.models.db_models.sub_category import SubCategory
 
 
 def is_valid_uuid(input_str):
@@ -7,3 +14,78 @@ def is_valid_uuid(input_str):
         return str(uuid_obj) == input_str
     except ValueError:
         return False
+
+
+def validate_actual_price(actual_price: str):
+    if isinstance(actual_price, str):
+        actual_price = actual_price.strip()
+    actual_price = Decimal(actual_price)
+    if actual_price <= 0:
+        raise ECOMValueError(msg="Actual Price can not be zero or negative")
+    if actual_price >= 1000000:
+        raise ECOMValueError(msg="Price can not be greater than Rs.9,99,999")
+
+
+def validate_offer_price(offer_price: str, actual_price: str):
+    if isinstance(offer_price, str):
+        offer_price = offer_price.strip()
+    offer_price = Decimal(offer_price)
+
+    if isinstance(actual_price, str):
+        actual_price = actual_price.strip()
+    actual_price = Decimal(actual_price)
+
+    if offer_price <= 0:
+        raise ECOMValueError(msg="Offer Price can not be zero or negative")
+    if offer_price > actual_price:
+        raise ECOMValueError(msg="Offer Price can not be grater than actual price")
+
+
+def validate_category_type(category_type: str):
+    category_type = Category.objects.filter(name=category_type).first()
+    if not category_type:
+        raise ECOMValueError(msg="Category is not listed")
+
+
+def validate_subcategory_type(subcategory_type: str, category_type: str):
+    sub_category_type: SubCategory = SubCategory.objects.filter(
+        name=subcategory_type
+    ).first()
+    if not sub_category_type:
+        raise ECOMValueError(msg="Sub Category is not listed")
+    elif sub_category_type.category.name != category_type:
+        raise ECOMValueError(msg="Category & Sub Category are not matching")
+
+
+def validate_product_name(product_name: str):
+    if product_name == "":
+        raise ECOMValueError(msg="Product name should have length more than 5")
+    if product_name and len(product_name) < 5:
+        raise ECOMValueError(msg="Product name should have length more than 5")
+
+
+def validate_product_image(product_image: str):
+    if product_image == "":
+        raise ECOMValueError(msg="Product image can not be a empty string")
+    if product_image and len(product_image) < 4:
+        raise ECOMValueError(msg="Product image URL is invalid")
+
+
+def validation_product_image_list(product_image_list: List[str]):
+    if product_image_list:
+        for image in product_image_list:
+            validate_product_image(image)
+
+
+def validate_product_request_data(data: dict):
+    validate_actual_price(data.get("actual_price"))
+    validate_offer_price(
+        offer_price=data.get("offer_price"), actual_price=data.get("actual_price")
+    )
+    validate_category_type(data.get("category"))
+    validate_subcategory_type(
+        subcategory_type=data.get("subcategory"), category_type=data.get("category")
+    )
+    validate_product_name(data.get("product_name"))
+    validate_product_image(data.get("image"))
+    validation_product_image_list(data.get("product_image_list"))
